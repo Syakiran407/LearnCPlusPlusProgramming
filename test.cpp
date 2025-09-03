b670ba70226856ee356e8b11d362a10f41d2cfdb
Mahmud Syakiran <mahmudsyakiran407@gmail.com>
2025-09-03 04:03:45 +0000

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>  // for round
+using namespace std;
+
+int main() {
+    
+    double money;
+
+    cout << "Enter the money amount: " << endl;
+
+    cin >> money;
+
+    int amount = round(money * 100);
+
+    cout << "You have " << amount << endl;
+
+    int tenDollar = amount / 1000;
+    amount %= 1000;
+
+    cout << "You need " << tenDollar << " Ten Dollar Notes" << endl;
+
+    int fiveDollar = amount / 500;
+    amount %= 500;
+
+    cout << "You need " << fiveDollar << " Five Dollar Notes" << endl;
+
+    int oneDollar = amount / 100;
+    amount %= 100;
+
+    cout << "You need " << oneDollar << " One Dollar Notes" << endl;
+
+    return 0;
+}
