979ce5d93ae6d1233bcec06f7206919e88342dfe
Mahmud Syakiran <mahmudsyakiran407@gmail.com>
2025-09-03 05:12:20 +0000

    Added some code to money_counter file

--- money_counter.cpp
+++ money_counter.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <cmath>  // for round
 using namespace std;
 
 int main()
 {
 
     cout << "Hello from money counter program!" << endl;
+
+    double money;
+
+    cout << "Enter the money amount: " << endl;
+
+    cin >> money;
+
+    int amount = round(money * 100);
+
+    cout << "You have " << amount << endl;
+
+    int tenDollar = amount / 1000;
+    amount %= 1000;
+
+    cout << "You need " << tenDollar << " Ten Dollar Notes" << endl;
+
+    int fiveDollar = amount / 500;
+    amount %= 500;
+
+    cout << "You need " << fiveDollar << " Five Dollar Notes" << endl;
+
+    int oneDollar = amount / 100;
+    amount %= 100;
+
+    cout << "You need " << oneDollar << " One Dollar Notes" << endl;
 
     return 0;
 }
